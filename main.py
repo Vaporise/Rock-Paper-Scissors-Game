292aaad15f04e878283dba118aa5e5fe0a8b70cb
Vaporise <draxdragonfire@googlemail.com>
2025-11-25 16:36:44 +0900

    Added the main game loop. Still needs some work on the win/lose/draw and game loop

--- main.py
+++ main.py
@@ -3,11 +3,31 @@ import random
 def main():
 
     game_loop = True
-    print("Rock, paper, or scissors? (r/p/s): ")
-    choice = input().lower()
+    
+    
+
     while game_loop == True:
+        
+        choices = ["r", "p", "s"]
+
+        print("Rock, paper, or scissors? (r/p/s): ")
+        
+        choice = input().lower()
+
         if choice == "r" or choice == "p" or choice == "s": 
-            cpu_choice = 
+            cpu_choice = random.choice(choices)
             print(f"You chose {choice}")
+            print(f"The CPU chose {cpu_choice}")
+            if choice == "r" and cpu_choice == "s":
+                print("You win!")
+            elif choice == "r" and cpu_choice == "r":
+                print("It's a draw!")
+            elif choice == "r" and cpu_choice == "p":
+                print ("You lose")
+            game_loop = False
         else:
             print("Invalid choice")
+            game_loop = True
+
+if __name__ == "__main__":
+    main()
