c56a2b957c6dfbdc55a99f4662b160a4015b7a1b
Vaporise <draxdragonfire@googlemail.com>
2025-11-25 16:56:00 +0900

    Added variables to capture wins/draws/loses

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ def main():
 
     game_loop = True
     
-    
+    wins = 0
+    draws = 0
+    loses = 0
 
     while game_loop == True:
         
@@ -20,10 +22,13 @@ def main():
             print(f"The CPU chose {cpu_choice}")
             if choice == "r" and cpu_choice == "s":
                 print("You win!")
+                wins += 1
             elif choice == "r" and cpu_choice == "r":
                 print("It's a draw!")
+                draws += 1
             elif choice == "r" and cpu_choice == "p":
                 print ("You lose")
+                loses += 1
             game_loop = False
         else:
             print("Invalid choice")
