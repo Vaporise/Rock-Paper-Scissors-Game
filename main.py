99909d5c88cf49d6de210e2c7b9bb2b6c3519ccc
Vaporise <draxdragonfire@googlemail.com>
2025-12-01 13:52:05 +0900

    Made the code for working out win conditions WA more concise and easier to read and follow, added in a scoreboard and play again with counters

--- main.py
+++ main.py
@@ -2,37 +2,52 @@ import random
 
 def main():
 
-    game_loop = True
+    game_loop = True #Set the state of the game loop
     
-    wins = 0
-    draws = 0
-    loses = 0
+    wins = 0 #Wins counter
+    draws = 0 #Draws counter
+    losses = 0 #Loses counter
 
-    while game_loop == True:
+    choices = ["r", "p", "s"] #List of the choices.
+    rules = { # a dictionary of the win conditions.
+        "r": "s", #rock beats scissors
+        "p": "r", #paper beats rock
+        "s": "p" #scissors beats paper
+    }
+
+    while game_loop == True: # Game loop starts
         
-        choices = ["r", "p", "s"]
+        
 
         print("Rock, paper, or scissors? (r/p/s): ")
         
-        choice = input().lower()
+        choice = input().lower() #Take input and convert it to lower case.
 
-        if choice == "r" or choice == "p" or choice == "s": 
+        if choice in choices: #redid this code to make it easier to read and more concise. Before I had the options listens now it just works with my choices list.
             cpu_choice = random.choice(choices)
             print(f"You chose {choice}")
             print(f"The CPU chose {cpu_choice}")
-            if choice == "r" and cpu_choice == "s":
-                print("You win!")
-                wins += 1
-            elif choice == "r" and cpu_choice == "r":
+
+            if choice == cpu_choice: #if the choices match its a draw.
                 print("It's a draw!")
                 draws += 1
-            elif choice == "r" and cpu_choice == "p":
-                print ("You lose")
+            elif rules[choice] == cpu_choice: #if the value matches the cpu choice then the player make a winning choice. e.g rules[r] = s and opponent choice s.
+                print("You win!")
+                wins += 1
+            else:
+                print("You lose!") #
                 loses += 1
-            game_loop = False
+
+            print(f"Wins:{win}, Draws: {draws}, Losses: {losses}") #Print the current score out.
+
+            #Play again code
+            play_again = input("Play again? (y/n)").lower()
+            if play_again == "n":
+                game_loop = False
+
+
         else:
             print("Invalid choice")
-            game_loop = True
 
 if __name__ == "__main__":
-    main()
+    main() 
